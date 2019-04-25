0a052c17c15a788b67b5eab24c70169d3cc408a1
Rick Knowlton <richard.d.knowlton@gmail.com>
2019-04-25 13:33:04 -0400

    v 1.2.2

--- functions/index.js
+++ functions/index.js
@@ -57,7 +57,7 @@ function postToSlack(url, commits, repo) {
 // This is the URL that we will callback and send the content of the updated data node.
 // As an example we're using a Request Bin from http://requestb.in
 // TODO: Make sure you create your own Request Bin and change this URL to try this sample.
-const WEBHOOK_URL = 'https://enfw3dnvhngto.x.pipedream.net';
+const WEBHOOK_URL = '<INSERT SLACK WEBHOOK HERE>';
 
 // Reads the content of the node that triggered the function and sends it to the registered Webhook
 // URL.
@@ -66,7 +66,10 @@ exports.webhook = functions.database.ref('/hooks/{hookId}').onCreate(async (snap
     uri: WEBHOOK_URL,
     method: 'POST',
     json: true,
-    body: snap.val(),
+    body: { 
+        text: "This works."
+    },
+    attachments: [snap.val()],
     resolveWithFullResponse: true,
   });
   if (response.statusCode >= 400) {
